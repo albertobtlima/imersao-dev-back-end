6400bef11a097edb6d948cb6a1826c95d1eae25f
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-11-22 14:07:51 -0300

    add; instalike-main

--- instalike-back/src/routes/posts-routes.js
+++ instalike-back/src/routes/posts-routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import cors from "cord";
 
 import {
   listarPosts,
@@ -7,6 +8,11 @@ import {
   uploadImagem,
   atualizarNovoPost,
 } from "../controllers/posts-controller.js";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 //Para uso no Windows
 /* const storage = multer.diskStorage({
@@ -19,10 +25,13 @@ import {
 });
 const upload = multer({ dest: "./uploads", storage}); */
 
+//Para uso no Mac e Linux
 const upload = multer({ dest: "./uploads" });
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cors(corsOptions));
+
   app.get("/posts", listarPosts);
   app.post("/posts", postarNovoPost);
   app.post("/upload", upload.single("imagem"), uploadImagem);
